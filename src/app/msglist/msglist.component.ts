8864ab6223ceb39cbc774eb75c8fe72c9ffb1486
Skachkov_Work <k.skachkov@mail.ru>
2021-06-18 11:21:15 +0300

    1806 day

--- src/app/msglist/msglist.component.ts
+++ src/app/msglist/msglist.component.ts
@@ -4,6 +4,7 @@ import { MsgList } from '../classes/msgList';
 import { MsgRead } from '../classes/msgRead';
 import { Adresat } from '../classes/adresat';
 import { Write } from '../classes/write';
+import { Theme } from '../classes/theme';
 
 
 @Component({
@@ -20,8 +21,9 @@ export class MsglistComponent implements OnInit {
   showOut:boolean = false;
   color:boolean = true;
   color2:boolean = false;
-  writemsg:boolean=false;
-  writemsgId:string="0"
+  color3:boolean = false;
+  writemsg:boolean = false;
+  writemsgId:string = "0"
   letr:string = "НОВОГО"
   proc:string = "EXEC MQL_M_MSG_LIST ?";
   param:string = "0004000000025400025400010002120210620 23:59:59.999";
@@ -36,41 +38,43 @@ export class MsglistComponent implements OnInit {
   srv2: MsgRead[] = [];
   adresat:Adresat[] = [];
   write:Write[] = [];
+  theme:Theme[] = [];
   selectedRowIndex:any;
   selectedRowIndex2:any;
   my1:string = "54";
   my2:string = "54";
   q:number = 0;
+
   adritem:string="Укажите Адресат";
+  amsg_kod:string="";
+  id_str_videl:string="0"
+  id_str_jirfont:string="0"
   dthema:boolean = true;
   dkomu:boolean = true;
-  
-  
-  
-  
-
+    
   constructor(private getmsg:MsggetService) {}
 
   clickInBox(){
-    if (this.showIn == false){
       this.showIn = true;
       this.showOut = false;
-    }
+      this.writemsg = false;
+    
     this.color = true;
     this.color2 = false;
-    
+    this.color3 = false;
     
   }
 
   clickOutBox(){
     
-    if (this.showOut == false){
       this.showOut = true;
       this.showIn = false;
+      this.writemsg = false;
       //this.showOut=!this.showOut;
-    }
-    this.color = false;
-    this.color2 = true;
+      
+      this.color = false;
+      this.color2 = true;
+      this.color3 = false;
 
     const formData : FormData = new FormData();
     formData.append('param', this.paramout);
@@ -132,6 +136,9 @@ export class MsglistComponent implements OnInit {
     this.showCard2 = false;
     this.showIn = false;
     this.showOut = false;
+    this.color = false;
+    this.color2 = false;
+    this.color3 = true;
     //Получаем список Адресатов
       const formData : FormData = new FormData();
       var l1 = (this.my1.length.toString().padStart(4, "0"))+this.my1; 
@@ -146,6 +153,7 @@ export class MsglistComponent implements OnInit {
     
     this.writemsgId = "0";
     this.adritem = adr.name2;
+    this.amsg_kod = adr.name3;
     //запрос на получение служебных данных окна написания сообщения
     var l1 = (adr.name1.length.toString().padStart(4, "0"))+adr.name1;
     var l2 = (this.my2.length.toString().padStart(4, "0"))+this.my2;
@@ -170,6 +178,19 @@ export class MsglistComponent implements OnInit {
     }
     });
 
+}
+
+  ThemeClick(){
+    const formData : FormData = new FormData();
+    var l1 = (this.amsg_kod.length.toString().padStart(4, "0"))+this.amsg_kod;
+    var l2 = (this.my2.length.toString().padStart(4, "0"))+this.my2;
+    var l3 = "00010";
+    var l4 = (this.id_str_videl.length.toString().padStart(4, "0"))+this.id_str_videl;
+    var l5 = (this.id_str_jirfont.length.toString().padStart(4, "0"))+this.id_str_jirfont;
+    var newparam = l1+l2+l3+l4+l5; 
+    formData.append('param', newparam);
+    formData.append('proc', "EXEC MQL_M_MSG_FORSEL_01");
+    this.getmsg.getMessages(formData).subscribe((data:any) => this.theme=data["MsgList"]);
 }
 
 
